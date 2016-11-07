d74854dd25724449d06665a67e446762b5a3b836
denesnori <denes.nori@yahoo.com>
2016-11-07 22:59:13 +0000

    push request

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 var http = require('http');
 var fs = require('fs');
 var path = require('path');
+var querystring = require('querystring');
 
 function handler(request,response){
   var endpoint = request.url;
   var extension = path.extname(endpoint).substring(1);
   console.log(endpoint);
   if(endpoint === "/"){
-
     response.writeHead(200,{"Content-type":"text/html"});
 
     fs.readFile(__dirname + '/public/index.html',function(err,file){
@@ -18,7 +18,18 @@ function handler(request,response){
         }
       response.end(file);
     });
-  } else {
+  } else if( endpoint === '/create-post'){
+      response.writeHead(303,{"Location":"/index.html"});
+      var allData = "";
+      request.on('data', function(chunkOfData){
+          allData += chunkOfData;
+      });
+      request.on('end',function(){
+        var convertedData = querystring.parse(allData);
+        response.end();
+      });
+  }
+  else {
     response.writeHead(200,{"Content-type":"text/"+extension});
 
     fs.readFile(__dirname +'/public'+ endpoint,function(err,file){
