9ddc67ff430cf4d808caf2dc67425d6a77f28550
denesnori <denes.nori@yahoo.com>
2016-11-07 21:19:26 +0000

    add handler function

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 var http = require('http');
-var server = http.createServer();
+
+var message = "Learning node at night is fun!";
+
+function handler(request,response){
+  response.writeHead(200,{"Content-type":"text/html"});
+  response.write(message);
+  response.end();
+}
+
+var server = http.createServer(handler);
 server.listen(3000,function(){
   console.log('Server listens to requests on port 3000.');
 });
